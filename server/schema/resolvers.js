9dca308aced82e9f964db2e71ce712d972679ddd
Pedro Machado <machadop1407@gmail.com>
2021-11-02 11:05:31 -0700

    lab7

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -23,15 +23,6 @@ const resolvers = {
       return movie;
     },
   },
-  User: {
-    favoriteMovies: () => {
-      return _.filter(
-        MovieList,
-        (movie) =>
-          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
-      );
-    },
-  },
 
   Mutation: {
     createUser: (parent, args) => {
