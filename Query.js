9d88e51e5bc3b4efb85c4459e9ff9f743ca45115
Pedro Machado <machadop1407@gmail.com>
2021-11-02 15:34:03 -0700

    commit

---
+++ Query.js
@@ -0,0 +1,15 @@
+/* Write a fragment for the type MOVIE described below. The fragment should include the fields: title, director
+ *
+ * type Movie {
+ *   id: ID!
+ *   title: String!
+ *   yearOfRelease: Int!
+ *   director: String!
+ * }
+ */
+
+const gqlFragment = `
+
+
+
+`;
--- server/index.js
+++
@@ -1,9 +0,0 @@
-const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("./schema/type-defs");
-const { resolvers } = require("./schema/resolvers");
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({ url }) => {
-  console.log(`YOUR API IS RUNNING AT: ${url} :)`);
-});
